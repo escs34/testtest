0ef663d1659cdbb4e320abde51fbdfce6a066c6a
jiisoo <jsmin0415@gmail.com>
2019-01-09 19:16:10 +0900

    update changes

--- run_spark.sh
+++ run_spark.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-sudo docker run -dit --name spark-client --network spark-net -p 22006:22 --add-host=master:10.0.2.2 --cpu-shares 1024 --memory 12g kmubigdata/ubuntu-spark /bin/bash
+sudo docker run -dit --name spark240 --network test-net hadoop-spark /bin/bash
