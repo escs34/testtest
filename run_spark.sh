2de4a0385bd50fcfbe37042b9e04e073495a919f
손성수 <sungsu7437@naver.com>
2019-06-03 11:28:09 +0900

    java,sbt path added, 8080 port added

--- run_spark.sh
+++ run_spark.sh
@@ -14,66 +14,68 @@ portNumber=1
 masterIP=""
 
 if [[ -z "$3" ]]; then
-	echo "format is wrong"
-	echo "./add_studnet network-name student-from student-to"
-	echo "ex) ./add_student my-net 1 5"
-	echo "network is my-net, student from 1, student to 5"
-	exit 0
+        echo "format is wrong"
+        echo "./add_studnet network-name student-from student-to"
+        echo "ex) ./add_student my-net 1 5"
+        echo "network is my-net, student from 1, student to 5"
+        exit 0
 fi
 
 
 numOfContaiers=$2
 while [ $numOfContaiers != $(($3+1)) ];
 do
-	SLAVE_COMMAND="$STUDENT_FRONT student$numOfContaiers --network $1 --ip $IP$(($numOfContaiers+1)) --memory 2.5g"
-	if [ $portNumber -lt 10 ]; then
-                SLAVE_COMMAND="$SLAVE_COMMAND -p 2210$portNumber:22 -p 2220$portNumber:18080"
+        SLAVE_COMMAND="$STUDENT_FRONT student$numOfContaiers --network $1 --ip $IP$(($numOfContaiers+1)) --memory 2.5g"
+        if [ $portNumber -lt 10 ]; then
+                SLAVE_COMMAND="$SLAVE_COMMAND -p 2210$portNumber:22 -p 2220$portNumber:8080 -p 2230$portNumber:18080"
         else
-                SLAVE_COMMAND="$SLAVE_COMMAND -p 221$portNumber:22 -p 222$portNumber:18080"
+                SLAVE_COMMAND="$SLAVE_COMMAND -p 221$portNumber:22 -p 222$portNumber:8080 -p 223$portNumber:18080"
         fi
 
-	if [ $(($numOfContaiers % 6)) == 1 ]; then
-		tempNum=$(($numOfContaiers+1))
-		passwd=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 10 | head -n 1)
-		echo "student$numOfContaiers passwd = $passwd"
-	fi
+        if [ $(($numOfContaiers % 6)) == 1 ]; then
+                tempNum=$(($numOfContaiers+1))
+                passwd=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 10 | head -n 1)
+                echo "student$numOfContaiers passwd = $passwd"
+        fi
 
-	numOfAddHosts=0
-	while [ $numOfAddHosts != 6 ];
-	do
-		if [ $numOfAddHosts != 0 ]; then
-	                SLAVE_COMMAND="$SLAVE_COMMAND $ADDHOST $SLAVE$(($numOfAddHosts)):$IP$(($tempNum + $numOfAddHosts))"
-	                numOfAddHosts=$(($numOfAddHosts + 1))
-		else
-			SLAVE_COMMAND="$SLAVE_COMMAND $ADDHOST master:$IP$(($tempNum + $numOfAddHosts))"
-			masterIP=$SLAVE$(($numOfAddHosts + 1))
-			numOfAddHosts=$(($numOfAddHosts + 1))
-		fi
-	done
+        numOfAddHosts=0
+        while [ $numOfAddHosts != 6 ];
+        do
+                if [ $numOfAddHosts != 0 ]; then
+                        SLAVE_COMMAND="$SLAVE_COMMAND $ADDHOST $SLAVE$(($numOfAddHosts)):$IP$(($tempNum + $numOfAddHosts))"
+                        numOfAddHosts=$(($numOfAddHosts + 1))
+                else
+                        SLAVE_COMMAND="$SLAVE_COMMAND $ADDHOST master:$IP$(($tempNum + $numOfAddHosts))"
+                        masterIP=$SLAVE$(($numOfAddHosts + 1))
+                        numOfAddHosts=$(($numOfAddHosts + 1))
+                fi
+        done
 
+        SLAVE_COMMAND="$SLAVE_COMMAND --cpu-shares 1024"
 
+        $SLAVE_COMMAND $COMMAND_LAST
+        sudo docker exec student$numOfContaiers bash -c "echo 'root:$passwd' | chpasswd"
+        sudo docker exec student$numOfContaiers bash -c "sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config"
+        sudo docker exec student$numOfContaiers bash -c "service ssh restart"
 
-	SLAVE_COMMAND="$SLAVE_COMMAND --cpu-shares 1024"
-
-	$SLAVE_COMMAND $COMMAND_LAST
-	sudo docker exec student$numOfContaiers bash -c "echo 'root:$passwd' | chpasswd"
-	sudo docker exec student$numOfContaiers bash -c "sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config"
-	sudo docker exec student$numOfContaiers bash -c "service ssh restart"
-
-	sudo docker exec student$numOfContaiers bash -c "echo 'export HADOOP=/usr/local/hadoop' >> ~/.bashrc"
-	sudo docker exec student$numOfContaiers bash -c "echo 'export PATH=\$HADOOP/bin:\$PATH' >> ~/.bashrc"
+        sudo docker exec student$numOfContaiers bash -c "echo 'export HADOOP=/usr/local/hadoop' >> ~/.bashrc"
+        sudo docker exec student$numOfContaiers bash -c "echo 'export PATH=\$HADOOP/bin:\$PATH' >> ~/.bashrc"
 
         sudo docker exec student$numOfContaiers bash -c "echo 'export HADOOP_HOME=\$HADOOP' >> ~/.bashrc"
         sudo docker exec student$numOfContaiers bash -c "echo 'export SPARK_HOME=/usr/local/spark' >> ~/.bashrc"
         sudo docker exec student$numOfContaiers bash -c "echo 'export PATH=\$PATH:\$SPARK_HOME/bin:\$SPARK_HOME/sbin' >> ~/.bashrc"
 
-	sudo docker exec student$numOfContaiers bash -c "source ~/.bashrc"
+        sudo docker exec student$numOfContaiers bash -c "echo 'expoert JAVA_HOME=/usr/java/default' >> ~/.bashrc"
+        sudo docker exec student$numOfContaiers bash -c "echo 'export PATH=\$PATH:\$JAVA_HOME/bin' >> ~/.bashrc'"
+        sudo docker exec student$numOfContaiers bash -c "echo 'export PATH=\$PATH:/usr/share/sbt/bin' >> ~/.bashrc"
 
-	sudo docker exec student$numOfContaiers bash -c "cp /etc/hosts ~/hosts.new ; sed -i '\$d' ~/hosts.new ; cp -f ~/hosts.new /etc/hosts"
+        sudo docker exec student$numOfContaiers bash -c "source ~/.bashrc"
 
-	SLAVE_COMMAND=''
-	numOfContaiers=$(($numOfContaiers+1))
-	coreNumber=$(($coreNumber+1))
-	portNumber=$(($portNumber+1))
-	masterIP=""
+        sudo docker exec student$numOfContaiers bash -c "cp /etc/hosts ~/hosts.new ; sed -i '\$d' ~/hosts.new ; cp -f ~/hosts.new /etc/hosts ; rm ~/hosts.new"
+
+        SLAVE_COMMAND=''
+        numOfContaiers=$(($numOfContaiers+1))
+        coreNumber=$(($coreNumber+1))
+        portNumber=$(($portNumber+1))
+        masterIP=""
 done
