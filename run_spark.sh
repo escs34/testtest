87fd83c8dc338dc7475067d6376bafa5a51b62e9
손성수 <sungsu7437@naver.com>
2019-06-19 11:53:49 +0900

    Comments added

--- run_spark.sh
+++ run_spark.sh
@@ -16,7 +16,7 @@ masterIP=""
 
 if [[ -z "$4" ]]; then
         echo "format is wrong"
-        echo "./add_studnet network-name student-from student-to"
+        echo "./add_student network-name student-from student-to"
         echo "ex) ./add_student my-net 1 10 5"
         echo "network is my-net, student from 1, student to 10, 1 master 4 slaves"
         exit 0
@@ -27,14 +27,14 @@ numOfContaiers=$2
 sparkGroup=$4
 while [ $numOfContaiers != $(($3+1)) ];
 do
-
+        ##Setting master and worker node's CPU core number, memory size
         if [ $(($numOfContaiers % $sparkGroup)) == 1 ]; then
                 SLAVE_COMMAND="$STUDENT_FRONT student$numOfContaiers --network $1 --ip $IP$(($numOfContaiers+1)) -m 8192m --cpu-shares 4096"
         else
                 SLAVE_COMMAND="$STUDENT_FRONT student$numOfContaiers --network $1 --ip $IP$(($numOfContaiers+1)) -m 4096m --cpu-shares 2048"
         fi
         
-        ##port forwarding for ssh only master containers
+        ##port forwarding for ssh only master containers. Numbering starts from 22101
         if [ $(($numOfContaiers % $sparkGroup)) == 1 ]; then
                 if [ $(($numOfContaiers / $sparkGroup + 1)) -lt 10 ]; then
                         SLAVE_COMMAND="$SLAVE_COMMAND -p 2210$(($numOfContaiers / $sparkGroup + 1)):22"
@@ -48,12 +48,15 @@ do
         #        SLAVE_COMMAND="$SLAVE_COMMAND -p 221$numOfContaiers:22 -p 222$numOfContaiers:8080 -p 223$numOfContaiers:18080"
         #fi
 
+        ##Creating password for containers
         if [ $(($numOfContaiers % $sparkGroup)) == 1 ]; then
                 tempNum=$(($numOfContaiers+1))
                 passwd=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 10 | head -n 1)
                 echo "student$numOfContaiers passwd = $passwd"
         fi
 
+        ##Adding ip information on /etc/hosts. If you use master node through docker exec command, below code does not need.
+        ##But if you use master node through ssh, there is no way to get ip information so below command needed.
         numOfAddHosts=0
         while [ $numOfAddHosts != $sparkGroup ];
         do
@@ -68,12 +71,16 @@ do
         done
 
         #SLAVE_COMMAND="$SLAVE_COMMAND --cpu-shares 4096"
-
+        
+        ##Run creating docker command
         $SLAVE_COMMAND $COMMAND_LAST
+        
+        ##Setting password the container and changing ssh configure to allow root login on ssh
         sudo docker exec student$numOfContaiers bash -c "echo 'root:$passwd' | chpasswd"
         sudo docker exec student$numOfContaiers bash -c "sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config"
         sudo docker exec student$numOfContaiers bash -c "service ssh restart"
 
+        ##Adding hadoop, spark pathes. Same reason for addHosts
         sudo docker exec student$numOfContaiers bash -c "echo 'export HADOOP=/usr/local/hadoop' >> ~/.bashrc"
         sudo docker exec student$numOfContaiers bash -c "echo 'export PATH=\$PATH:\$HADOOP/bin:\$HADOOP_HOME/sbin' >> ~/.bashrc"
 
@@ -96,9 +103,11 @@ do
 
         sudo docker exec student$numOfContaiers bash -c "source ~/.bashrc"
 
+        ##Deleting first line of /etc/hosts and hadoop/workers, spark/conf/slaves files.
         sudo docker exec student$numOfContaiers bash -c "cp /etc/hosts ~/hosts.new ; sed -i '\$d' ~/hosts.new ; cp -f ~/hosts.new /etc/hosts ; rm ~/hosts.new"
         sudo docker exec student$numOfContaiers bash -c "sed -i '1d' /usr/local/hadoop/etc/hadoop/workers ; sed -i '1d' /usr/local/spark/conf/slaves"
 
+        ##Adding slaves and workers names on configure files.
         numOfSlaves=1
         while [ $numOfSlaves != $sparkGroup ];
         do
